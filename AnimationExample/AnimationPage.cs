2960771e4b37932f0ef93e7d47ff726c62c7c5fd
kseyko35 <seyfiercan35@hotmail.com>
2020-05-15 00:38:53 +0300

    * .gitignore: First Commit
    * AnimationExample.sln:
    * App.xaml:
    * Main.cs:
    * App.xaml.cs:
    * MainPage.xaml:
    * Info.plist:
    * AssemblyInfo.cs:
    * MainPage.xaml.cs:
    * AnimationPage.cs:
    * AppDelegate.cs:
    * visa.png:
    * Entitlements.plist:
    * AnimationExample.csproj:
    * MainActivity.cs:
    * Default.png:
    * Default@2x.png:
    * AboutAssets.txt:
    * AssemblyInfo.cs:
    * AnimationExample.iOS.csproj:
    * Default-568h@2x.png:
    * Default-Portrait.png:
    * AssemblyInfo.cs:
    * styles.xml:
    * colors.xml:
    * visa.png:
    * Tabbar.xml:
    * AboutResources.txt:
    * Toolbar.xml:
    * LaunchScreen.storyboard:
    * Default-Portrait@2x.png:
    * AndroidManifest.xml:
    * Resource.designer.cs:
    * icon.png:
    * icon.png:
    * AnimationExample.Android.csproj:
    * icon.png:
    * icon.png:
    * icon.png:
    * icon.xml:
    * Icon58.png:
    * Icon60.png:
    * Icon76.png:
    * Icon80.png:
    * Icon87.png:
    * Icon29.png:
    * Icon20.png:
    * Icon40.png:
    * icon_round.xml:
    * Icon152.png:
    * Icon120.png:
    * Icon167.png:
    * Icon180.png:
    * Icon1024.png:
    * Contents.json:
    * launcher_foreground.png:
    * launcher_foreground.png:
    * launcher_foreground.png:
    * launcher_foreground.png:
    * launcher_foreground.png:

---
+++ AnimationExample/AnimationPage.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using Xamarin.Forms;
+namespace AnimationExample
+{
+    public class AnimationPage : ContentPage
+    {
+        public AnimationPage()
+        {
+            RelativeLayout layout = new RelativeLayout();
+            Image mImage = new Image
+            {
+                Source = "visa.png"
+            };
+
+
+            Button mRotateButton = new Button
+            {
+                Text = "Rotater",
+                VerticalOptions = LayoutOptions.Center,
+                HorizontalOptions = LayoutOptions.Center,
+                Opacity= 0
+
+            };
+   
+            mImage.GestureRecognizers.Add(new TapGestureRecognizer // Bu sekilde resmimize tap eventi eklemis olduk
+            {
+               
+                Command = new Command(async (obj) =>
+                { 
+                    while (true)
+                    {
+                        await mImage.LayoutTo(new Rectangle(1, 150, mImage.Width, mImage.Height), 1250, Easing.SpringIn);
+                        //await mImage.LayoutTo(new Rectangle(1, 100, mImage.Width, mImage.Height), 1250, Easing.SpringIn);
+                        await mImage.RotateXTo(360, 1250, Easing.Linear);
+                        await mImage.RotateXTo(0, 1250, Easing.Linear);
+                    }
+                    //await mImage.ScaleTo(1 , 250, Easing.CubicIn);
+                    //await mImage.ScaleTo(1, 1250, Easing.CubicInOut);
+                    //await mImage.LayoutTo(new Rectangle(1 ,150, mImage.Width, mImage.Height),1250,Easing.SpringIn);
+                    //await mImage.RotateXTo(360, 1250, Easing.Linear);
+                    ////await DisplayAlert("Title", "Ok", "Cancel");
+                    //await mRotateButton.FadeTo(1, 250, Easing.Linear);
+
+                } ) 
+
+            } );
+            //mRotateButton.IsVisible = true; 
+         
+            
+   
+            mRotateButton.Clicked += async (sender, args) => { await mImage.RelRotateTo(360, 1000); };
+
+            layout.Children.Add(mImage, Constraint.Constant(0) , Constraint.RelativeToParent(
+                parent => { return parent.Height - 50; }), Constraint.RelativeToParent(
+                parent => { return parent.Width; })); // Resmi ekranin ortasina koyabilmemiz icin
+
+            layout.Children.Add(mRotateButton,Constraint.Constant(0),
+                Constraint.RelativeToParent(parent => { return parent.Height - 100; }),
+                Constraint.RelativeToParent(parent => { return parent.Width; }));
+            Content = layout;
+        }
+
+    }
+}
---
+++ AnimationExample/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AnimationExample
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AnimationPage();
+        
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
