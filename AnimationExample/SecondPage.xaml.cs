be9cf3a47d044242d8ca069a42a10ca1f8a1e54a
kseyko35 <seyfiercan35@hotmail.com>
2020-05-16 20:19:01 +0300

    Added Second page

---
+++ AnimationExample/SecondPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace AnimationExample
+{
+    public partial class SecondPage : ContentPage
+    {
+        public SecondPage()
+        {
+            InitializeComponent();
+        }
+
+        void onClickImage(System.Object sender, System.EventArgs e)
+        {
+            var tap = e as TappedEventArgs;
+            DisplayAlert("Gesture Example!", tap.Parameter.ToString(), "Cancel");
+            
+        }
+
+        void onClickLabel(System.Object sender, System.EventArgs e)
+        {
+            var tap = (TappedEventArgs)e;
+         
+            var label =(Label)sender;
+            
+            DisplayAlert("Gesture Example!", tap.Parameter.ToString(), "Cancel");
+            label.Text = "You clicked Me!";
+            label.BackgroundColor = Color.FromHex("#3497db");
+            label.TextColor = Color.White;
+
+        }
+    }
+}
